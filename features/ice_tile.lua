ca5b17ef547ac66697c7f506e17ac190ce24209c
adam <adam7792@gmail.com>
2025-11-21 08:32:40 +0100

    Add ice tile feature with dynamic terrain properties
    Defines properties for an ice tile feature in the game.This feature represents a traversable ice tile that can be destroyed to change pathing. Part of dynamic ice mechanic.

---
+++ features/ice_tile.lua
@@ -0,0 +1,16 @@
+return {
+  description = "Ice tile (dynamic terrain)",
+  blocking = false,
+  damage = 1000,
+  reclaimable = false,
+  footprintX = 1,
+  footprintZ = 1,
+  object = "blank.s3o",
+  customParams = {
+    grounddecaltex = "ice_tile.png",
+    grounddecalsizex = 1,
+    grounddecalsizey = 1,
+    grounddecaldecay = 0.1,
+    ice_state = "solid",
+  },
+}
