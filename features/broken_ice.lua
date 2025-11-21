001605b91ee0e7dd518b0e0a7e55a3203de6ff18
adam <adam7792@gmail.com>
2025-11-21 08:33:59 +0100

    Add broken ice tile feature
    Defines properties for a broken ice tile, including damage and visual parameters.

---
+++ features/broken_ice.lua
@@ -0,0 +1,16 @@
+return {
+  description = "Broken ice tile (fragile)",
+  blocking = false,
+  damage = 1000,
+  reclaimable = false,
+  footprintX = 1,
+  footprintZ = 1,
+  object = "blank.s3o",
+  customParams = {
+    grounddecaltex = "broken_ice.png",
+    grounddecalsizex = 1,
+    grounddecalsizey = 1,
+    grounddecaldecay = 0.1,
+    ice_state = "broken",
+  },
+}
